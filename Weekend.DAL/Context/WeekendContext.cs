04966798b4f4f736ca19d25ac5dee6d8c92c0f4d
Koan Miki <aomiki.iki@gmail.com>
2020-03-30 00:02:26 +0400

    Remote PostgreSQL

--- Weekend.DAL/Context/WeekendContext.cs
+++ Weekend.DAL/Context/WeekendContext.cs
@@ -26,7 +26,7 @@ namespace Weekend.DAL.Context
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseNpgsql(LocalPgDB);
+                optionsBuilder.UseNpgsql(HerokuDB);
            //     optionsBuilder.UseSqlServer("Data Source=LAPTOP-BBTQSDD5\\SQLEXPRESS;Integrated Security=True; Database=Weekend; Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
