cc4bab14f0c95aa71e456bf5cf282b5453f52542
Koan Miki <aomiki.milky@icloud.com>
2020-08-22 12:43:55 +0400

    Adjusting message page to the server

---
+++ WeekendProject.WebSite/Controllers/MessagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Weekend.WebSite.Controllers
+{
+    public class MessagesController : Controller
+    {
+        [HttpGet("/Messages")]
+        public IActionResult Messages()
+        {
+            return View();
+        }
+    }
+}
